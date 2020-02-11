1640f30dfd862b89fc2ae49ee5706548d377ebbd
Tawhid <433tawhidul@bitbucket.org>
2020-02-12 01:49:50 +0600

    homePartiallyUpdated

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,32 @@
 <!-- Footer -->
-<footer class="py-5 bg-dark">
+<footer class="bg-dark footer">
     <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-5">
+                <h4 class="text-white">Address</h4>
+                <div class="text-muted">
+                    <small><b>House Name: </b>some house no</small><br>
+                    <small><b>Road No: </b>some Road no</small><br>
+                    <small><b>City: </b>some city no</small>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <h4 class="text-white">Contact</h4>
+                <div class="text-muted font-14">
+                    <small><b>Cell: </b>some house no</small><br>
+                    <small><b>Phone: </b>(+880) 12309xxxx, <b>Local: </b>16215</small><br>
+                    <small><b>E-Mail: </b>something@mail.com</small>
+                </div>
+        </div>
+            <div class="col-md-1"></div>
+        </div>
+        <hr />
+        <div class="col copyright">
+            <p class="text-center text-white">Copyright &copy;
+                    <span class="logo-name">Moner_Daktar</span>, 2020
+                </p>
+        </div>
     </div>
     <!-- /.container -->
 </footer>
