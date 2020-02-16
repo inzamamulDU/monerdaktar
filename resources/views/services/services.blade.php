2e7d553f2734d60b83cc1ea084e92588f2e71f4d
Tawhid <433tawhidul@bitbucket.org>
2020-02-17 01:58:48 +0600

    left files are added

---
+++ resources/views/services/services.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+
+@section('content')
+    <div>
+        <h1 class="centered padding-top-20">Our Services</h1>
+        <div class="padding-top-20">
+            <p>We provide a complete package of mental health care through our digital platform. First time in Bangladesh we provide our client the freedom of choice from the list of our famous psychiatrists and seek help whenever they wish. We provide psychiatric evaluation by most reputed psychiatrists for all types of mental disorders such as Depression, Anxiety, OCD, Schizophrenia, ADHD, Drug Addiction, Behavioral addiction, Dementia. You will receive your e-prescription from the psychiatrist. We also provide psychotherapy session by the famous psychologists of the country; the psychiatrist can refer our client of psychotherapy session whenever required.  However, you can also ask for various scientific tips, suggestions and counselling for various day to day problems and crisis such as how to promote optimum development of your child, the parenting, the relationship problem, adjustment problem, sexual problem, communication skill problem and improve our skill such as parenting skill, social skill, stress management, emotional and professional development.  We promote most updated evidence based practice.</p>
+            <p>You can create your own Electronic Health Record and we will take care of that for life long. You will be able to use them whenever it requires.</p>
+            <div>
+                <h3>24/7 direct consultation to available Psychiatrists and Psychologists</h3>
+                <p> Online Psychiatric Consultation: We provide the psychiatric consultation from the most reputed psychiatrist in the country having MD or FCPS and other advanced degrees.</p>
+                <p>Online Psychotherapy and Counselling: Our expert Clinical Psychologists and Councilors provide evidence based services for our clients.</p>
+            </div>
+            <div>
+                <h3>Self Assessment</h3>
+                <p>Online Psycho-social Assessment:</p>
+                <p>Assess Depression</p>
+                <p>Assess Your Personality Trait</p>
+                <p>Assess overall your Stress, Anxiety, Depression, …</p>
+            </div>
+            <div>
+                <h3>Information related nearest mental health hospitals, mental health professionals and pharmacy</h3>
+            </div>
+        </div>
+    </div>
+@endsection
